9e6eea6370a72fb918e547bd611e46721430cd8e
ivan4000 <sxln1@mail.ru>
2018-10-02 16:22:12 +0300

    JL12 incapsulation

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+
+    private String name = "Max";
+    private String mr = "mr. ";
+    public String getName() {
+        return name;
+    }
+
+    public String getMr() {
+        return mr;
+    }
+    public String getMrName(){
+        return mr+name;
+    }
+}
